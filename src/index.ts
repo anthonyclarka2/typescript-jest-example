16190a995dda4e1cd6e3e4b5e27d7eb9721a6177
Anthony Jarvis-Clark <69362332+aclarknexient@users.noreply.github.com>
2024-09-23 11:44:18 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const PORT = process.env.PORT ?? "8080";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Engage test opentelemetry response");
+  res.send("Test opentelemetry response");
   console.log("Request for /");
 });
 
